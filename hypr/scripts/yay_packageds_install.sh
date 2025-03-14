1178001ad42ad3dbf6e898917018742d2c42132e
jpedroccruz <joaopedroccruz.dev@gmail.com>
2025-03-13 21:33:14 -0300

    update installation files

--- hypr/scripts/yay_packageds_install.sh
+++ hypr/scripts/yay_packageds_install.sh
@@ -1,1 +1,1 @@
-yay -S waypaper wlogout clipse hyprlock hypridle starship nwg-look hyprshot cava-git 
+yay -S waypaper wlogout clipse hyprlock hypridle starship nwg-look hyprshot cava-git graphite-gtk-theme 
